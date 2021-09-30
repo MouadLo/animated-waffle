ad0a51ba19cac0c4c5416196f03c3eba13ae0206
Mouad Lousimi <mouad.lousimi@etud.iga.ac.ma>
2021-09-30 03:07:17 +0100

    Transform - backface visibility and Flip Product

--- src/components/App.js
+++ src/components/App.js
@@ -4,24 +4,11 @@ import Home from './Home/Home';
 import ProductList from './ProductList/ProductList';
 
 function App() {
-	const ProductsDiv = [
-		<div>One</div>,
-		<div className="two">Two</div>,
-		<div>Three</div>,
-	];
-
 	return (
 		<div className="App">
-			<div className="parent">
-				<div>1</div>
-				<div>2</div>
-				<div>3</div>
-				<div>4</div>
-				<div>5</div>
-				<div>6</div>
-				<div>7</div>
-				<div>8</div>
-				<div>9</div>
+			<div className="box">
+				<div className="face front">Front</div>
+				<div className="face back">Back</div>
 			</div>
 		</div>
 	);
